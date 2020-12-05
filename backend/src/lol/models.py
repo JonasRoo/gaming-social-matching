7490bb6cde1048fef5d5bd547a10f06bfeb44e13
JonasRoo <jonas-roosen@t-online.de>
2020-12-05 16:36:50 +0100

    Celery cleanup | new updated static ddragon fetching (unscheduled)

--- backend/src/lol/models.py
+++ backend/src/lol/models.py
@@ -1,5 +1,8 @@
 from typing import Dict, Union
+from datetime import datetime, timedelta
+import warnings
 from django.db import models
+from common_utils import json_utils
 
 
 class Game(models.Model):
@@ -29,7 +32,18 @@ class Game(models.Model):
 
 # V -------------- ddragon -------------- V
 class Version(models.Model):
+    """
+    Represents a response of the version API.
+    A 'version' string is necessary to query the DDragon endpoints.
+    Example: `10.24.1`.
+    WARNING: versions are region specific!
+    Polled from the Riot API:
+        -> list of all versions (DESC): https://ddragon.leagueoflegends.com/api/versions.json
+        -> versions per realm: https://ddragon.leagueoflegends.com/realms/na.json
+    """
+
     date_added = models.DateTimeField(auto_now_add=True, db_index=True)
+    # TODO(jonas): add region field as enum
     item = models.CharField(max_length=64)
     rune = models.CharField(max_length=64)
     mastery = models.CharField(max_length=64)
@@ -39,52 +53,121 @@ class Version(models.Model):
     map = models.CharField(max_length=64)
     language = models.CharField(max_length=64)
     sticker = models.CharField(max_length=64)
+    cdn = models.URLField()
 
     class Meta:
         ordering = ["-date_added"]
 
-    @property
-    def api_model_map() -> Dict[str, str]:
-        return {
-            "profileicon": "profile_icon",
-        }
+    @classmethod
+    def _get_api_model_map(cls) -> Dict[str, str]:
+        d = {}
+        for field in cls._meta.get_fields():
+            if field.name not in ["id", "pk", "date_added", "cdn"]:
+                d[f"n-{field.name.replace('_', '')}"] = field.name
+        d["cdn"] = "cdn"
+        return d
 
     @classmethod
     def _from_api_dict(cls, api_response: Dict[str, str]) -> "Version":
-        mapper = cls.api_model_map
+        api_response = json_utils.flatten_dict_by_joining(api_response)
+        formatted_response = {}
+        mapper = cls._get_api_model_map()
         for k, v in mapper.items():
-            api_response[v] = api_response.pop(k)
-        return cls(**api_response)
+            formatted_response[v] = api_response.pop(k)
+        return cls(**formatted_response)
+
+    @property
+    def last_version(self):
+        version = self.objects.latest(field_name="date_added")
+        if datetime.utcnow() - version.date_added > timedelta(days=1):
+            warnings.warn(
+                f"Latest version update is more than 1 day old. (Last pulled: {version.date_added})"
+            )
+        return version
 
 
-# TODO(jonas): how do I dynamically "generate" the splash / sprite URL prefixes
+# TODO(jonas): how to dynamically "generate" the splash / sprite URL prefixes
 class Champion(models.Model):
     """
     Represents a champion within League of Legends.
     Polled from the Riot API:
-        ->
+        -> http://ddragon.leagueoflegends.com/cdn/{{version}}/data/en_US/champion.json
     """
 
     id = models.IntegerField(primary_key=True)
-    version = models.ForeignKey(to=Version, to_field="champion", db_column="version")
+    updated_at = models.DateTimeField(auto_now=True)
+    version = models.CharField(max_length=64)
+    internal_name = models.CharField(max_length=64, blank=False, null=False)
     name = models.CharField(max_length=64, blank=False, null=False)
     title = models.CharField(max_length=128, blank=True, null=True)
     splash = models.URLField(blank=True, null=True)
-    sprite = models.URLField(blank=True, null=True)
+    loading = models.URLField(blank=True, null=True)
+    square = models.URLField(blank=True, null=True)
     tags = models.CharField(max_length=64, blank=True, null=True)
 
     class Meta:
         unique_together = ("id", "version")
 
+    # The "0" suffix corresponds to the base skin (higher number for skins...)
+    def _construct_full_splash_url(self, version: Version) -> str:
+        return f"{version.cdn}/img/champion/splash/{self.internal_name}_0.jpg"
+
+    def _construct_full_loading_url(self, version: Version) -> str:
+        return f"{version.cdn}/img/champion/loading/{self.internal_name}_0.jpg"
+
+    # Champion squares are NOT skin dependent
+    def _construct_full_square_url(self, version: Version) -> str:
+        return f"{version.cdn}/{version.champion}/img/champion/{self.internal_name}.png"
+
+    @classmethod
+    def _get_api_model_map(cls) -> Dict[str, str]:
+        return {
+            "key": "id",
+            "version": "version",
+            "id": "internal_name",
+            "name": "name",
+            "title": "title",
+            "tags": "tags",
+        }
+
+    @classmethod
+    def _from_single_champ_dict(
+        cls,
+        single_champ_dict: Dict[str, str],
+        mode: str = "instance",
+    ) -> "Champion":
+        formatted_response = {}
+        mapper = cls._get_api_model_map()
+        for k, v in mapper.items():
+            formatted_response[v] = single_champ_dict.pop(
+                " ".join(k) if isinstance(k, (list, tuple)) else k
+            )
+
+        if mode == "instance":
+            return cls(**formatted_response)
+        elif mode == "dict":
+            return formatted_response
+        else:
+            raise AttributeError(f"invalid mode!")
+
+    def save(self, version: Version = None, *args, **kwargs):
+        if version:
+            self.splash = self._construct_full_splash_url(version)
+            self.loading = self._construct_full_loading_url(version)
+            self.sprite = self._construct_full_square_url(version)
+        super(Champion, self).save(*args, **kwargs)
+
 
 class Queue(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=64, blank=False, null=False)
+    description = models.CharField(max_length=128, blank=True, null=True)
+    notes = models.CharField(max_length=128, blank=True, null=True)
 
 
 class Item(models.Model):
     id = models.IntegerField(primary_key=True)
-    version = models.ForeignKey(to=Version, to_field="item", db_column="version")
+    version = models.CharField(max_length=64)
     name = models.CharField(max_length=128, blank=False, null=False)
     description = models.CharField(max_length=256, blank=True, null=True)
     icon = models.URLField(blank=True, null=True)
---
+++ backend/src/lol/riot_interface/ddragon/data_util.py
@@ -0,0 +1,37 @@
+from typing import List, Optional
+from lol.models import Version, Champion
+from lol.riot_interface.client_secrets import X_RIOT_TOKEN
+from riotwatcher import LolWatcher
+
+_DEFAULT_REGION = "euw1"
+
+
+def fetch_static_data(also_fetch_queues: Optional[bool] = False):
+    watcher = LolWatcher(api_key=X_RIOT_TOKEN)
+    version = fetch_and_save_version(watcher=watcher)
+    fetch_and_write_champs(watcher=watcher, version=version)
+
+
+def fetch_and_save_version(
+    watcher: LolWatcher, for_region: Optional[str] = None
+) -> Version:
+    for_region = for_region or _DEFAULT_REGION
+    versions_resp = watcher.data_dragon.versions_for_region(for_region)
+    version = Version._from_api_dict(api_response=versions_resp)
+    version.save()
+    return version
+
+
+def fetch_and_write_champs(watcher: LolWatcher, version: Version):
+    champs_resp = watcher.data_dragon.champions(version=version.champion)
+    for raw_champ_data in champs_resp["data"].values():
+        champ_dict = Champion._from_single_champ_dict(raw_champ_data, mode="dict")
+        # TODO(jonas): this is a little less ugly.
+        unique_checks = {"id": champ_dict["id"], "version": champ_dict["version"]}
+        remaining_attrs = {
+            k: v for k, v in champ_dict.items() if k not in unique_checks
+        }
+        champ, _ = Champion.objects.update_or_create(
+            **unique_checks, defaults=remaining_attrs
+        )
+        champ.save(version=version)
